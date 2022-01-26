6163a975979267bde233cca3e0386e4df156248b
Mohd Saif Farooqui <42074096+mohdsaif91@users.noreply.github.com>
2022-01-27 00:05:59 +0530

    rest is pending commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,135 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+import MedicensImage from "./assets/medical.png";
+import { data } from "./util/data";
 
 function App() {
+  const [search, setSearch] = useState(false);
+  const [medicen] = useState([...data]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <div className="social-call">
+          <div className="phone">
+            <div className="contact">
+              <div className="country-text">USA</div>
+              <div className="number-container">
+                <a href="tel:+917869961901">call +1 888 6821109</a>
+                <a href="tel:+917869961901">text +1 678 7223433</a>
+              </div>
+            </div>
+            <div className="contact">
+              <div className="country-text">Austrila</div>
+              <div className="number-container">
+                <a href="tel:+917869961901">call +61 272 021 899</a>
+                <a href="tel:+917869961901">text +61 437 070 227</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="row v-center">
+            <div className="header-item item-left">
+              <div className="logo">
+                <a href="#">Matrix Pharmacy</a>
+              </div>
+              <div className="search-bar">
+                <div className="search-input">
+                  <input type="text" placeholder="Search For Product" />
+                  <a href="#" className="search-cancel">
+                    <i className="fas fa-times"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="header-item item-center">
+              <div className="menu-overlay"></div>
+              <nav className="menu">
+                <div className="mobile-menu-head">
+                  <div className="go-back">
+                    <i className="fas fa-angle-left"></i>
+                  </div>
+                  <div className="current-menu-title"></div>
+                  <div className="mobile-menu-close">&times;</div>
+                </div>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </header>
+
+      <ul id="#adaptive" className="cs-hidden">
+        <li className="item-a">
+          <div className="full-slider-box f-slide-1">
+            <h1 className="typing">Sale is live !! </h1>
+            <div className="slider-text-container">
+              <div className="f-slider-text">
+                <span className="sub-text">
+                  We are runnig promotions prices are low like never before
+                </span>
+                <strong>
+                  $0.50 cents per Pill/Tablet/Capsule
+                  <br />
+                </strong>
+              </div>
+              <div className="img-container">
+                <img className="medical-image" src={MedicensImage} />
+              </div>
+            </div>
+          </div>
+        </li>
+      </ul>
+
+      <section className="product-list">
+        <div className="container">
+          <h2 className="product-title">Featured Products</h2>
+          <div className="row">
+            <div className="item">
+              <div className="product">
+                {medicen.map((m) => (
+                  <div ley={m.id} className="product-container">
+                    <div className="product-thumb">
+                      <img src={m.imageName} alt="" className="pro-thumb-img" />
+                    </div>
+
+                    <div className="product-info">
+                      <h1 className="product-name">{m.name}</h1>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <header className="header">
+        <div className="social-call">
+          <div className="phone">
+            <div className="contact">
+              <div className="country-text">USA</div>
+              <div className="number-container">
+                <a href="tel:+917869961901">call +1 888 6821109</a>
+                <a href="tel:+917869961901">text +1 678 7223433</a>
+              </div>
+            </div>
+            <div className="contact">
+              <div className="country-text">Austrila</div>
+              <div className="number-container">
+                <a href="tel:+917869961901">call +61 272 021 899</a>
+                <a href="tel:+917869961901">text +61 437 070 227</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="extra-space">
+          <div className="email-container">
+            <div>Email</div>
+            <a href="mailto: billing.matrixpharmacy@gmail.com">
+              billing.matrixpharmacy@gmail.com
+            </a>
+          </div>
+        </div>
       </header>
     </div>
   );
---
+++ src/util/data.js
@@ -0,0 +1,79 @@
+import azithromycin250 from "../assets/azithromycin-250.jpeg";
+import azithromycin500 from "../assets/azithromycin-500.jpeg";
+import cenforce100 from "../assets/cenforce-100.jpeg";
+import cenforce150 from "../assets/cenforce-150.jpeg";
+import cenforce200 from "../assets/cenforce-200.jpeg";
+import hetquenil200 from "../assets/hetquenil-200.jpeg";
+import hetquenil400 from "../assets/hetquenil-400.jpeg";
+import iverheal3 from "../assets/iverheal-3.jpeg";
+import iverheal6 from "../assets/iverheal-6.jpeg";
+import iverheal12 from "../assets/iverheal-12.jpeg";
+import painosoma350 from "../assets/painosoma-350.jpeg";
+import painosoma500 from "../assets/painosoma-500.jpeg";
+import testastroneblue from "../assets/testosterone-blue.jpeg";
+import testastronered from "../assets/testosterone-red.jpeg";
+import vidalista20 from "../assets/vidalista-20.jpeg";
+import vidalista40 from "../assets/vidalista-40.jpeg";
+import vidalista60 from "../assets/vidalista-60.jpeg";
+import vidalista80 from "../assets/vidalista-80.jpeg";
+import vidalistblack80 from "../assets/vidalista-black-80.jpeg";
+import vilitra20 from "../assets/vilitra-20.jpeg";
+import vilitra40 from "../assets/vilitra-40.jpeg";
+import vilitra60 from "../assets/vilitra-60.jpeg";
+
+export const data = [
+  {
+    id: 1,
+    name: "Azithromycin 250 MG",
+    imageName: azithromycin250,
+  },
+  {
+    id: 2,
+    name: "Azithromycin 500 MG",
+    imageName: azithromycin500,
+  },
+  { id: 3, name: "Viagra 100 mg", imageName: cenforce100 },
+  { id: 4, name: "Viagra 150 mg", imageName: cenforce150 },
+  { id: 5, name: "Viagra 200 mg", imageName: cenforce200 },
+  {
+    id: 6,
+    name: "Hydroxychloroquine 200 MG",
+    imageName: hetquenil200,
+  },
+  {
+    id: 7,
+    name: "Hydroxychloroquine 200 MG",
+    imageName: hetquenil400,
+  },
+  { id: 8, name: "Ivermectin 3 MG", imageName: iverheal3 },
+  { id: 9, name: "Ivermectin 6 MG", imageName: iverheal6 },
+  { id: 10, name: "Ivermectin 12 MG", imageName: iverheal12 },
+  { id: 11, name: "Soma 350 MG", imageName: painosoma350 },
+  { id: 12, name: "Soma 500 MG", imageName: painosoma500 },
+  {
+    id: 13,
+    name: "Sustanon 250 MG 1 ML Vials",
+    imageName: testastroneblue,
+  },
+  {
+    id: 14,
+    name: "Testosterone Enanthate Depot 25 MG 1 ML",
+    imageName: testastronered,
+  },
+  { id: 15, name: "Cialis 20 MG", imageName: vidalista20 },
+  { id: 16, name: "Vidalista 40 MG", imageName: vidalista40 },
+  { id: 17, name: "Cialis 60 MG", imageName: vidalista60 },
+  {
+    id: 18,
+    name: "Cialis 80 mg Yellow",
+    imageName: vidalista80,
+  },
+  {
+    id: 19,
+    name: "Cialis 80 mg black",
+    imageName: vidalistblack80,
+  },
+  { id: 20, name: "Levitra 20 MG", imageName: vilitra20 },
+  { id: 21, name: "Levitra 40 MG", imageName: vilitra40 },
+  { id: 22, name: "Levitra 60 MG", imageName: vilitra60 },
+];
